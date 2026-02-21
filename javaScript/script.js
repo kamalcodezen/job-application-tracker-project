05c6fcd2abd5e08525b5087561eb06e6db33043b
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-22 03:08:04 +0530

    filter button color toggle complete

--- javaScript/script.js
+++ javaScript/script.js
@@ -13,6 +13,8 @@ const interviewFilterBtn = document.getElementById("interview-filter-btn");
 const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
 
 
+
+
 let interviewList = [];
 let rejectedList = [];
 // access length job count
@@ -25,3 +27,65 @@ calculateJobCount();
 
 
 // filter available job button toggle
+function showFilterBtn(id) {
+
+    allFilterBtn.classList.remove("bg-[#3b82f6]", "text-white");
+    interviewFilterBtn.classList.remove("bg-[#3b82f6]", "text-white");
+    rejectedFilterBtn.classList.remove("bg-[#3b82f6]", "text-white");
+
+    const selectedId = document.getElementById(id);
+    selectedId.classList.add("bg-[#3b82f6]", "text-white");
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// all time active button 
+document.addEventListener("DOMContentLoaded", function () {
+    showFilterBtn("all-filter-btn");
+});
